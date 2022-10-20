d879beb70d6b55e1c33f9e7c4253d39776c0df5e
vogel <over.castrillon22396@ucaldas.edu.co>
2022-10-20 11:39:37 -0500

    Adición de MaterializeCSS, con esto se crea la carpeta funciones en assets, dentro al archivo JS se adicionan las mismas, sea en este caso para el navbar y footer

---
+++ src/assets/functions/materialize.js
@@ -0,0 +1,6 @@
+/* TODO: Adición de funcionalidades para el uso de MaterializeCSS */
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav')
+    var instances = M.Sidenav.init(elems, {})
+})
