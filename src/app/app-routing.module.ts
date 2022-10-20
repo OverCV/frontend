37183a10116cd9ac577cae7e0e75fdb98985444d
vogel <over.castrillon22396@ucaldas.edu.co>
2022-10-20 09:46:37 -0500

    Adición de enrutamiento inicial para uso predeterminado de Home y en caso de acceso a una ruta desconocida se entrará a alguno de los componentes de Error.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './public/errors/not-found/not-found.component';
+import { HomeComponent } from './public/master-page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    /* Así redireccionaremos cuando el usuario ingrese a una ruta desconocida, como estas van en orden irá de última al chequear */
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
