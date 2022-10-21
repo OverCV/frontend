4229c717444fe92b4a5ae9a3c18dff86ab62fc9c
vogel <over.castrillon22396@ucaldas.edu.co>
2022-10-20 19:09:43 -0500

    Adición de MaterializeCSS para desarrollo en visualización de la parte Home, Login y seguridad, se sigue en implementación de recuperación de contraseña e ingreso al sistema.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/security/login/login.component';
+import { ResetPasswordComponent } from './modules/security/reset-password/reset-password.component';
 import { NotFoundComponent } from './public/errors/not-found/not-found.component';
 import { HomeComponent } from './public/master-page/home/home.component';
 
@@ -12,6 +14,14 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: '/home'
+  },
+  {
+    path: 'reset-password',
+    component: ResetPasswordComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     /* Así redireccionaremos cuando el usuario ingrese a una ruta desconocida, como estas van en orden irá de última al chequear */
--- src/assets/functions/materialize.js
+++ src/assets/functions/materialize.js
@@ -1,6 +1,23 @@
 /* TODO: Adición de funcionalidades para el uso de MaterializeCSS */
-
+/**
+ * Funcionalidad de menú hamburguesa.
+ */
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav')
     var instances = M.Sidenav.init(elems, {})
+})
+/**
+ * Funcionalidad de imágenes
+**/
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed')
+    var instances = M.Materialbox.init(elems, {})
+})
+
+/**
+ * Funcionalidad de autocompletado en formularios.
+**/
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.autocomplete')
+    var instances = M.Autocomplete.init(elems, {})
 })
